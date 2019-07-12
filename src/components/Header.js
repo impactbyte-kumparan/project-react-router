5961b05a0e1f03284eb0c2003a429852ffadfd70
M Haidar Hanif <git@mhaidarhanif.com>
2019-07-12 14:26:57 +0700

    Style navigation and link to pokemon page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import styled from 'styled-components'
+
+const Navigation = styled.nav`
+  ul {
+    padding-left: 10px;
+  }
+  ul li {
+    display: inline-block;
+    margin-right: 10px;
+  }
+`
+
 const Header = () => {
   return (
-    <nav>
+    <Navigation>
       <ul>
         <li>
           <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/pokemon">Pokemon</Link>
         </li>
         <li>
           <Link to="/about">About</Link>
@@ -17,8 +32,11 @@ const Header = () => {
         <li>
           <Link to="/topics">Topics</Link>
         </li>
+        <li>
+          <Link to="/search">Search</Link>
+        </li>
       </ul>
-    </nav>
+    </Navigation>
   )
 }
 
