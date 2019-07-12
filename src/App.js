20b3ac7e16ec8f7b436436585e658f4a6e30ab51
M Haidar Hanif <git@mhaidarhanif.com>
2019-07-12 14:00:17 +0700

    Configure App with Router and Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Header from './components/Header'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import Users from './pages/Users'
+import Topics from './pages/Topics'
 
 const App = () => {
-  return <div>CONTENT</div>
+  return (
+    <Router>
+      <div>
+        <Header />
+
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/users" component={Users} />
+        <Route path="/topics" component={Topics} />
+      </div>
+    </Router>
+  )
 }
 
 export default App
