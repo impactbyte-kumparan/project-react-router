0b69834ae49842bf52a6da8f3594e419abb9a5d2
M Haidar Hanif <git@mhaidarhanif.com>
2019-07-12 14:44:49 +0700

    Implement pokemon data into single Pokemon page

--- src/pages/Pokemon.js
+++ src/pages/Pokemon.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import queryString from 'query-string'
 
-const Pokemon = props => {
-  return <div>Pokemon #{props.match.id}</div>
+import pokemons from '../data/pokemons.json'
+
+const Pokemon = ({ match, location }) => {
+  const id = Number(match.params.id)
+  const { types } = queryString.parse(location.search)
+
+  console.log(types)
+
+  const pokemon = pokemons.find(pokemon => pokemon.id === id)
+
+  return (
+    <div>
+      <Link to="/">Back</Link>
+      <h2>Pokemon #{pokemon.id}</h2>
+      <p>{pokemon.description}</p>
+      {types && <p>{JSON.stringify(pokemon.types)}</p>}
+    </div>
+  )
 }
 
 export default Pokemon
