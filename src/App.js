749e9fe5880e31d2c5425a83b594a098b8740ab7
M Haidar Hanif <git@mhaidarhanif.com>
2019-07-12 14:26:44 +0700

    Add and import route for pokemon and search

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 
 import Home from './pages/Home'
+import Pokemon from './pages/Pokemon'
 import About from './pages/About'
 import Users from './pages/Users'
 import Topics from './pages/Topics'
+import Search from './pages/Search'
 
 const App = () => {
   return (
@@ -15,9 +17,12 @@ const App = () => {
         <Header />
 
         <Route path="/" exact component={Home} />
+        <Route path="/pokemon" component={Home} />
+        <Route path="/pokemon/:id" component={Pokemon} />
         <Route path="/about" component={About} />
         <Route path="/users" component={Users} />
         <Route path="/topics" component={Topics} />
+        <Route path="/search" component={Search} />
       </div>
     </Router>
   )
